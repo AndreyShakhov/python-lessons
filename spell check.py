cb998d3fbfa6eeb9512351385edefb0dfeea7655
Administrator <9202888607@mail.ru>
2023-05-30 21:27:53 +0300

    Исправил подсчет суммы 5ти элементов

--- spell check.py
+++ spell check.py
@@ -1,10 +1,10 @@
 famous_words = set()
 unknown_words = set()
 
-for _ in range(int(input())): # формируем словарь известных слов
+for i in range(int(input())): # формируем словарь известных слов
     famous_words.add(input().strip().lower())
 
-for _ in range(int(input())): # формируем список слов для проверки
+for i in range(int(input())): # формируем список слов для проверки
     for word in input().strip().lower().split():
         if word not in famous_words: # слова отсутствующие в словаре известных слов - добавляем в словарь неизвестных
             unknown_words.add(word)
